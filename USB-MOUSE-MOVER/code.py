40347645e1dc2388bbdd285bdc0060f7ca36289a
Radosław Woźniak <r.wozniak@ultranet.net.pl>
2023-01-25 19:56:04 +0100

    Update code.py

--- USB-MOUSE-MOVER/code.py
+++ USB-MOUSE-MOVER/code.py
@@ -1,43 +1,55 @@
+
 import busio
 import board
 import digitalio
 import usb_hid
+import supervisor
+import microcontroller
 import terminalio
 import displayio
-import microcontroller
 from adafruit_display_text import label
 from adafruit_st7789 import ST7789
 from time import sleep
 from adafruit_hid.mouse import Mouse
 
-# First set some parameters used for shapes and text
-BORDER = 10
-FONTSCALE = 4
-BACKGROUND_COLOR = 0x00FF00  # Bright Green
-FOREGROUND_COLOR = 0xAA0088  # Purple
-TEXT_COLOR = 0xFFFF00
 
-mouse = Mouse(usb_hid.Device.MOUSE)
+
+print('code.py stsarted')
 
 # Release any resources currently in use for the displays
 displayio.release_displays()
-spi = busio.SPI(clock=board.GP18,MOSI=board.GP19)
+spi = busio.SPI(clock=board.GP18, MOSI=board.GP19)
 tft_cs = board.GP17
 tft_dc = board.GP16
 display_bus = displayio.FourWire(spi, command=tft_dc, chip_select=tft_cs)
-display = ST7789(display_bus, rotation=270, width=240, height=135, rowstart=40, colstart=53)
+display = ST7789(
+    display_bus, rotation=270, width=240, height=135, rowstart=40, colstart=53
+)
 
-led = digitalio.DigitalInOut(board.GP7)
-led.direction = digitalio.Direction.OUTPUT
+ledR = digitalio.DigitalInOut(board.GP6)
+ledR.direction = digitalio.Direction.OUTPUT
+ledR.value = True  # led is OFF
+ledG = digitalio.DigitalInOut(board.GP7)
+ledG.direction = digitalio.Direction.OUTPUT
+ledG.value = True  # led is OFF
+ledB = digitalio.DigitalInOut(board.GP8)
+ledB.direction = digitalio.Direction.OUTPUT
+ledB.value = True  # led is OFF
 
-button_x = digitalio.DigitalInOut(board.GP14)
-button_x.direction = digitalio.Direction.INPUT
-button_x.pull = digitalio.Pull.UP
+button_y = digitalio.DigitalInOut(board.GP15)
+button_y.direction = digitalio.Direction.INPUT
+button_y.pull = digitalio.Pull.UP
 
 button = True
 counter = int(0)
 
-def DispPrint(text):
+# First set some parameters used for shapes and text
+BORDER = 10
+BACKGROUND_COLOR = 0x00FF00  # Bright Green
+FOREGROUND_COLOR = 0xAA0088  # Purple
+TEXT_COLOR = 0xFFFF00
+
+def DispPrint(text, FONTSCALE = 2):
     # Make the display context
     splash = displayio.Group()
     display.show(splash)
@@ -55,34 +67,45 @@ def DispPrint(text):
     splash.append(text_group)
 
 
-
 def MySleep(sleeptime):
     sleepval = 0.1
     sleepcur = 0
-    while ( sleepcur < sleeptime and button_x.value is True ) :
+    while sleepcur < sleeptime and button_y.value is True:
         sleepcur = sleepcur + sleepval
         sleep(sleepval)
 
 
-while button:
-    counter = counter + 1
-    DispPrint('' + str(counter))
-    button = button_x.value
-    led.value = False
+def reset():
+    microcontroller.on_next_reset(microcontroller.RunMode.NORMAL)
+    microcontroller.reset()
     
+try:
+    mouse = Mouse(usb_hid.Device.MOUSE)
+except:
+    DispPrint("Error mouse init",FONTSCALE=2)
+    sleep(10)
+    reset()
+    
+while button:
+    counter +=1
+    DispPrint("" + str(counter),FONTSCALE=4)
+    button = button_y.value
+    ledG.value = False  # led is ON
     try:
         mouse.move(x=10)
         mouse.move(x=-10)
     except:
-        DispPrint('Error')
+        ledG.value = True  # led is OFF
+        ledR.value = False # led is ON
+        DispPrint("mouse move Error")
         sleep(10)
-        import microcontroller
-        microcontroller.reset()
-
-    sleep(.2)
-    led.value = True
+        reset()
+    sleep(0.2)
+    ledG.value = True
     MySleep(10)
 
-DispPrint('Resetting')
+ledB.value = False
+DispPrint("Resetting")
 sleep(10)
-microcontroller.reset()
+DispPrint("         ")
+reset()
